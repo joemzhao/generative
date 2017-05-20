f1c4338ff58946890c74269f5bd1f6e3692614ea
Mengjie <mengjie.zhao@outlook.com>
2017-05-20 19:49:14 +0200

    clear up

--- w2/SeqGAN/MLE_SeqGAN/read_params.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-with open("target_params.pkl", "rb") as f:
-    data = pickle.load(f)
-
-print type(data)
-print len(data)
-print data[0].shape
--- w2/SeqGAN/MLE_SeqGAN/save/parser.py
+++
@@ -1,9 +0,0 @@
-whole = []
-
-with open("real_data.txt") as f:
-    for item in f:
-        item = item.strip().split()
-        parse_line = [int(x) for x in item]
-        whole.extend(parse_line)
-f.close()
-print len(set(whole))
